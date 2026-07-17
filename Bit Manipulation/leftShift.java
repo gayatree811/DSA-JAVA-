90bceaacddf6dd08b9414a4d937adc97d5bee337
gayatree811 <gayatrigayatriparida133@gmail.com>
2026-07-17 23:37:56 +0530

    added newcode

---
+++ Bit Manipulation/leftShift.java
@@ -0,0 +1,8 @@
+public class leftShift {
+    public static void main(String[] args) {
+        int a =7;
+        System.out.println("a << 1 = " + (a << 1));
+        System.out.println("a << 2 = " + (a << 2));
+        System.out.println("a << 3 = " + (a << 3));
+    }
+}
---
+++ Bit Manipulation/rightShift.java
@@ -0,0 +1,11 @@
+public class rightShift {
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        System.out.println("Original Number : " + a);
+        System.out.println("a >> 1 = " + (a >> 1));
+        System.out.println("a >> 2 = " + (a >> 2));
+        System.out.println("a >> 3 = " + (a >> 3));
+    }
+}
